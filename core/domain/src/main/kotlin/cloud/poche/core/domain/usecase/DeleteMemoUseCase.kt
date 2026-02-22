5144786da460c639b7c5f5ad7ef2f8d6742ac636
Ryo Takeuchi <r_takeuchi@tatsutakein.jp>
2026-02-22 15:10:29 +0900

    feat(core-domain): メモの保存・削除ユースケースを追加
    - SaveMemoUseCase: メモの新規保存処理
    - DeleteMemoUseCase: メモの削除処理

---
+++ core/domain/src/main/kotlin/cloud/poche/core/domain/usecase/DeleteMemoUseCase.kt
@@ -0,0 +1,10 @@
+package cloud.poche.core.domain.usecase
+
+import cloud.poche.core.domain.repository.MemoRepository
+import javax.inject.Inject
+
+class DeleteMemoUseCase @Inject constructor(
+    private val memoRepository: MemoRepository,
+) {
+    suspend operator fun invoke(id: String) = memoRepository.deleteMemo(id)
+}
