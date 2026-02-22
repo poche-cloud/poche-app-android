671e6d34f4a48084a211d4392de3ae4a8954ccaa
Ryo Takeuchi <r_takeuchi@tatsutakein.jp>
2026-02-23 07:10:35 +0900

    feat(core-domain): メモ取得・更新の UseCase を追加
    - GetMemoByIdUseCase: ID によるメモ取得 (Flow)
    - UpdateMemoUseCase: メモ内容の更新

---
+++ core/domain/src/main/kotlin/cloud/poche/core/domain/usecase/GetMemoByIdUseCase.kt
@@ -0,0 +1,12 @@
+package cloud.poche.core.domain.usecase
+
+import cloud.poche.core.domain.repository.MemoRepository
+import cloud.poche.core.model.Memo
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetMemoByIdUseCase @Inject constructor(
+    private val memoRepository: MemoRepository,
+) {
+    operator fun invoke(id: String): Flow<Memo> = memoRepository.getMemo(id)
+}
---
+++ core/domain/src/main/kotlin/cloud/poche/core/domain/usecase/UpdateMemoUseCase.kt
@@ -0,0 +1,11 @@
+package cloud.poche.core.domain.usecase
+
+import cloud.poche.core.domain.repository.MemoRepository
+import cloud.poche.core.model.Memo
+import javax.inject.Inject
+
+class UpdateMemoUseCase @Inject constructor(
+    private val memoRepository: MemoRepository,
+) {
+    suspend operator fun invoke(memo: Memo) = memoRepository.updateMemo(memo)
+}
